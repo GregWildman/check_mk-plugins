dea6a450da14466b9be3503f5bc3efdaa768101b
Greg Wildman <greg@techno.co.za>
2018-07-13 12:43:52 +0200

    Notifications and WATO cutomizations

---
+++ techfu_extras/web/plugins/wato/wato_geo_fields.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# place this file to ~/local/share/check_mk/web/plugins/wato to get two new fields in the wato host properties.
+# this fields can be used to add Latiude and Longitude information. Usefull for the Nagvis Geomap
+
+declare_host_attribute(
+   NagiosTextAttribute(
+    "lat",
+    "_LAT",
+    "Latitude",
+    "Latitude",
+   ),
+   show_in_table = True,
+   show_in_folder = True,
+)
+
+declare_host_attribute(
+   NagiosTextAttribute(
+    "long",
+    "_LONG",
+    "Longitude",
+    "Longitude",
+   ),
+   show_in_table = True,
+   show_in_folder = True,
+)
+
+
