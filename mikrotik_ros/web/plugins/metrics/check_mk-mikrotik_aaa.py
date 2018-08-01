2826d9834c60560193c1431611ab256bd4c31307
Greg Wildman <greg@techno.co.za>
2018-08-01 17:43:13 +0200

    Added check for AAA sessions. Added perf-o-meters, fixed voltage description

---
+++ mikrotik_ros/web/plugins/metrics/check_mk-mikrotik_aaa.py
@@ -0,0 +1,5 @@
+
+check_metrics["check_mk-mikrotik_aaa"] = {
+    "ppp_sessions"   : { "name" : "active_sessions" }
+}
+
---
+++ mikrotik_ros/web/plugins/metrics/check_mk-mikrotik_health.py
@@ -0,0 +1,6 @@
+# in_voltage=17.9;;;;
+
+check_metrics["check_mk-mikrotik_health.voltage"] = {
+    "in_voltage" : { "name" : "voltage" }
+}
+
