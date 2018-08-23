d96f4a60be33ad3234949260fb7f9128317ed960
Greg Wildman <greg@techno.co.za>
2018-08-23 13:56:05 +0200

    metrics cleanup. Added MIB files for event console

--- intracom_radios/web/plugins/metrics/check_mk-intracom_radios.py
+++ intracom_radios/web/plugins/metrics/check_mk-intracom_radios.py
@@ -29,6 +29,7 @@
 # in=378935.231579;;;0;125000000 inucast=610.184834;;;; innucast=31.442246;;;; indisc=0.0933;;;; inerr=0;0.01;0.1;; out=375237.268903;;;0;125000000 outucast=708.710092;;;; outnucast=196.117511;;;; outdisc=0;;;; outerr=0;0.01;0.1;; outqlen=0;;;0;
 check_metrics["check_mk-intracom_eth"] = if_translation
 
+
 # output_power=-68.8dBm;;;; input_power=-64.8dBm;;;;
 check_metrics["check_mk-intracom_bsts"] = {
     "output_power" : { "name" : "output_signal_power_dbm" },
