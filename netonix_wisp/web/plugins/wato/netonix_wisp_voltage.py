0f233d3bdc5dc0805b86b0cb239468730d8b7f18
Greg Wildman <greg@techno.co.za>
2019-04-09 19:14:55 +0200

    Initial commit

---
+++ netonix_wisp/web/plugins/wato/netonix_wisp_voltage.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+register_check_parameters(
+    subgroup_environment,
+    "netonix_wisp_voltage",
+    _("Voltage levels for Netonix WISP Switches"),
+    Dictionary(
+        help = _("oltage Levels for Netonix WISP Switches."),
+        elements = [
+            ('warn', Float(title = _("Warning if below"), unit = "V", default_value = 25.0)),
+            ('crit', Float(title = _("Critical if below"), unit = "V", default_value = 24.0)),
+        ]
+    ),
+    TextAscii( title = _("Status Description"), allow_empty = True),
+    match_type = "dict",
+)
+
