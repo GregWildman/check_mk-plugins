993854a51c2b314d9ea4b5d8a133bd48af082690
Greg Wildman <greg@techno.co.za>
2020-02-04 15:22:33 +0200

    Add WATO rules for AC voltage levels

---
+++ alpha_cordex/web/plugins/wato/cordex_in_voltage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+register_check_parameters(
+    subgroup_environment,
+    "cordex_in_voltage",
+    _("Voltage levels for Cordex Rectifiers"),
+    Dictionary(
+        title=_("Limits"),
+        help = _("Voltage Levels for Cordex Rectifiers."),
+        elements = [
+            ('warn', Float(title = _("Warning if below"), unit = "V", default_value = 180.0)),
+            ('crit', Float(title = _("Critical if below"), unit = "V", default_value = 10.0)),
+        ]
+    ),
+    TextAscii(title=_("Status Description"), allow_empty=True),
+    match_type = "dict",
+)
+
